eea6bd8fb9fefd9c761506a5c36a818dcc597330
Netravati7 <67043742+Netravati7@users.noreply.github.com>
2021-03-04 11:54:59 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,25 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "😀",
+    name: "Grinning Face",
+    description:
+      "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor."
+  },
+  {
+    id: 2,
+    emoji: "😊",
+    name: "Smiling Face with Smiling Eyes",
+    description:
+      "A yellow face with smiling eyes and a broad, closed smile turning up to rosy cheeks. Often expresses genuine happiness and warm, positive feelings."
+  },
+  {
+    id: 3,
+    emoji: "😆",
+    name: "Grinning Squinting Face",
+    description:
+      "A yellow face with a broad, open smile and scrunched, X-shaped eyes. Often conveys excitement or hearty laughter."
+  }
+];
+
+export default emojipedia;
